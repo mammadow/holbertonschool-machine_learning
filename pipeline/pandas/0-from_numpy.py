8524676c418b24dd25490317b96fc287adb9358c
mammadow <mammadow.elnur@gmail.com>
2026-01-07 18:53:58 +0000

    from numpy

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This is a code for function that creates a pd.DataFrame from a np.ndarray
+"""
+import numpy as np
+import pandas as pd
+
+
+def from_numpy(array):
+    """
+    Function that creates pd.DataFrame from a np.ndarray
+    """
+    cols = [chr(ord('A') + i) for i in range(array.shape[1])]
+    return pd.DataFrame(array, columns=cols)
