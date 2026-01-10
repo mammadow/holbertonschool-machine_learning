af533be024a671d2e170977cb52a8d1e7c720d42
mammadow <mammadow.elnur@gmail.com>
2026-01-10 13:41:28 +0000

    from file

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,10 @@
+"""Loads data from a file into a pandas DataFrame"""
+
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    Loads data from a file as a pandas DataFrame.
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
