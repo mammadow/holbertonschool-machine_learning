d3abb456259e2f4a8d83ec41757f98816f7ae6f7
mammadow <mammadow.elnur@gmail.com>
2026-01-10 18:18:07 +0000

    concat

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+function def concat(df1, df2):
+"""
+
+
+def concat(df1, df2):
+    """
+    Function def concat(df1, df2): that takes two pd.DataFrame objects
+    """
+    index = __import__('10-index').index
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2.loc[:1417411920]
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
