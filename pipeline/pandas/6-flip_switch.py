e6b50f6377f4e4c321348cefb87ea72d9777f7eb
mammadow <mammadow.elnur@gmail.com>
2026-01-10 15:21:41 +0000

    flip switch

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function def flip_switch(df):
+"""
+import pandas as pd
+
+
+def flip_switch(df):
+    """
+    Function def flip_switch(df): that takes a pd.DataFrame
+    """
+    return df.sort_index(ascending=False).T
