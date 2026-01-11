9e9e3b6e0239ce665adaaae6a98fa964fcb0e9c7
mammadow <mammadow.elnur@gmail.com>
2026-01-11 17:10:26 +0100

    fix - pycodestyle

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,14 +7,17 @@ import matplotlib.pyplot as plt
 def bars():
     """code to plot a stacked bar graph"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(3)
 
     plt.bar(x, fruit[0], color='red', label="apples", width=0.5)
-    plt.bar(x, fruit[1], bottom=fruit[0], color='yellow', label="bananas", width=0.5)
-    plt.bar(x, fruit[2], bottom=fruit[0] + fruit[1], color='#ff8000', label="oranges", width=0.5)
-    plt.bar(x, fruit[3], bottom=fruit[0] + fruit[1] + fruit[2], color='#ffe5b4', label="peaches", width=0.5)
+    plt.bar(x, fruit[1], bottom=fruit[0], color='yellow',
+            label="bananas", width=0.5)
+    plt.bar(x, fruit[2], bottom=fruit[0] + fruit[1], color='#ff8000',
+            label="oranges", width=0.5)
+    plt.bar(x, fruit[3], bottom=fruit[0] + fruit[1] + fruit[2],
+            color='#ffe5b4', label="peaches", width=0.5)
 
     plt.xticks(x, ["Farrah", "Fred", "Felicia"])
     plt.ylabel("Quantity of Fruit")
