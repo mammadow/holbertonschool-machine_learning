65ff5195a0cff67ba52e067e7b285d6481c8f32a
mammadow <mammadow.elnur@gmail.com>
2026-01-18 16:52:46 +0000

    fix - pycodestyle

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,20 +6,20 @@ def poly_derivative(poly):
     """Calculates the derivatives of a polynomial"""
     if not isinstance(poly, list) or len(poly) == 0:
         return None
-    
+
     for coeff in poly:
         if not isinstance(coeff, (int, float)):
             return None
-    
+
     if len(poly) == 1:
         return [0]
-    
+
     derivative = []
 
     for power in range(1, len(poly)):
         derivative.append(poly[power] * power)
-    
+
     while len(derivative) > 1 and derivative[-1] == 0:
         derivative.pop()
-    
+
     return derivative
