f8697ea6abeb6e28d84496622e8a6ecebc7b570d
mammadow <mammadow.elnur@gmail.com>
2026-01-18 11:46:50 +0000

    n invalid

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,5 +2,6 @@
 """function that calculates i^2 from 1 to n"""
 
 def summation_i_squared(n):
-    sum = n*(n+1)*(2*n+1)/6
-    return sum
+    if not isinstance(n, int) or n < 1:
+        return None
+    return n*(n+1)*(2*n+1)/6
