1b6dac94314849dfbd564ed02a3e0091e61165f2
mammadow <mammadow.elnur@gmail.com>
2026-01-18 11:48:15 +0000

    fix - documentation

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""function that calculates i^2 from 1 to n"""
+"""Module for summation_i_squared function"""
 
 
 def summation_i_squared(n):
+    """function that calculates i^2 from 1 to n"""
     if not isinstance(n, int) or n < 1:
         return None
     return n*(n+1)*(2*n+1)/6
