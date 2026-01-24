9cfa174c99325e67cedbd22faa0233e75e2f5c7c
mammadow <mammadow.elnur@gmail.com>
2026-01-24 13:01:18 +0000

    shape

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    """
+    Returns the shape of a matrix as a list of integers
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
