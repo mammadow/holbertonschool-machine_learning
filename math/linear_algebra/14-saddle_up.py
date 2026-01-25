d3f5c8ef1b2fbe74d1aff55cdec091fd8cf023ab
mammadow <mammadow.elnur@gmail.com>
2026-01-25 15:28:41 +0000

    matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication using NumPy"""
+    return np.matmul(mat1, mat2)
