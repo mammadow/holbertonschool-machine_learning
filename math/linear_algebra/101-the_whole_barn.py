826ce80b584a54f44aaf8e6ab81a5c83913cc5ad
mammadow <mammadow.elnur@gmail.com>
2026-01-25 20:03:31 +0000

    fix

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -4,7 +4,7 @@
 
 def add_matrices(mat1, mat2):
     """Adds two matrices of any dimension"""
-    if type(mat1) != type(mat2):
+    if isinstance(mat1, list) != isinstance(mat2, list):
         return None
 
     if isinstance(mat1, list):
