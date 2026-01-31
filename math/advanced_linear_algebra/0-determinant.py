b3a85ccbd0ca962434e1c3a26a4a5d283dc8a880
mammadow <mammadow.elnur@gmail.com>
2026-01-31 18:18:04 +0000

    determinant

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""0-determinant.py"""
+
+
+def determinant(matrix):
+    """Compute the determinant of a square matrix."""
+
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        return 1
+
+    n = len(matrix)
+    if any(len(row) != n for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+    
+    if n == 1:
+        return matrix[0][0]
+
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    det = 0
+    for j in range(n):
+        minor = [row[:j] + row[j + 1:] for row in matrix[1:]]
+        det += ((-1) ** j) * matrix[0][j] * determinant(minor)
+    
+    return det
