8fb7dd719167502e0f61840146f52f5bc27489cc
mammadow <mammadow.elnur@gmail.com>
2026-02-05 17:10:48 +0000

    poisson

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -19,3 +19,19 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculates the PMF for a given number of successes (k)."""
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        fact = 1
+        for i in range(1, k+1):
+            fact *= i
+
+        e = 2.7182818285
+
+        return (e ** (-self.lambtha)) * (self.lambtha ** k) / fact
