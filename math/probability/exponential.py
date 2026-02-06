347a8c50d04d023001bc2f7e80e487fc6d21fb54
mammadow <mammadow.elnur@gmail.com>
2026-02-06 15:46:23 +0000

    exponential

---
+++ math/probability/exponential.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Exponential distribution module."""
+
+
+class Exponential:
+    """Represents an Exponential distribution."""
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initialize an Exponential distribution.
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain")
+
+            mean = sum(data) / len(data)
+            self.lambtha = float(1.0 / mean)
