9fdc3bf02e8fe9cdf864074e19365e4abd841880
mammadow <mammadow.elnur@gmail.com>
2026-02-10 14:50:05 +0400

    fix pycodestyle

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,7 +13,7 @@ def likelihood(x, n, P):
 
     if not isinstance(x, int) or x <= 0:
         raise ValueError("x must be an integer that is greater "
-                "than or equal to 0")
+                         "than or equal to 0")
 
     if x > n:
         raise ValueError("x cannot be greater than n")
