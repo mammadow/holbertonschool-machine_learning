6a27c80426b12b57443c1431ba36dc7eaf0e1c1a
mammadow <mammadow.elnur@gmail.com>
2026-03-06 20:16:24 +0400

    0-neuron.py

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron performing binary classification."""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron"""
+    def __init__(self, nx):
+        """
+        Initialize Neuron.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
