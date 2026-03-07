caaa269c3a7409714dad16af66482685f09dea03
mammadow <mammadow.elnur@gmail.com>
2026-03-08 01:15:56 +0400

    train upgrade

---
+++ supervised_learning/classification/7-neuron.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron performing binary classification."""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron"""
+    def __init__(self, nx):
+        """
+        Initialize Neuron.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculate the forward propagation of the neuron."""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculate the cost of the model using logistic regression.
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+                Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+                )
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluate the neuron's predictions.
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        Calculate one pass of gradient descent on the neuron.
+        """
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = (1 / m) * np.matmul(dZ, X.T)
+        db = (1 / m) * np.sum(dZ)
+        self.__W = self.__W - alpha * dW
+        self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05,
+              verbose=True, graph=True, step=100):
+        """Train the neuron."""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+
+        if verbose or graph:
+            if not isinstance(step, int):
+                raise TypeError("step must be an integer")
+            if step <= 0 or step > iteration:
+                raise ValueErro("step must be positive and <= iteration")
+
+        cost = []
+        steps = []
+
+        for i in range(iterations):
+            A = self.forward_prop(X)
+            cost = self.cost(Y, A)
+
+            if i % step == 0 or i == iteration:
+                if verbose:
+                    print(f"Cost after {i} iteration: {cost}")
+                if graph:
+                    costs.append(cost)
+                    steps.append(i)
+
+            if i < iterations:
+                self.gradient_descent(X, Y, A, alpha)
+
+        if graph:
+            plt.plot(steps, costs, 'b')
+            plt.xlabel("iteration")
+            plt.ylabel("cost")
+            plt.title("Training Cost")
+            plt.show()
+
+        return self.evaluate(X, Y)
