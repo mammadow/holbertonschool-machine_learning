eab8c9707d716f00cf87672c2d108f30fc556bf7
mammadow <mammadow.elnur@gmail.com>
2026-03-08 23:07:26 +0400

    one hot decode

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that contains one_hot_decode function"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Converts a one-hot matrix into a vector of labels"""
+    if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
+        return None
+
+    try:
+        return np.argmax(one_hot, axis=0)
+    except Exception:
+        return None
