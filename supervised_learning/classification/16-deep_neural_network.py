ace37d17cc4c36d775c5a3bf1fc2628ed479cd52
mammadow <mammadow.elnur@gmail.com>
2026-03-08 22:26:58 +0400

    fix

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -14,18 +14,22 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(nodes, int) and nodes > 0 for nodes in layers):
-            raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for i, nodes in enumerate(layers):
-            layer_num = i + 1
-            prev_nodes = nx if i == 0 else layers[i - 1]
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
 
-            self.weights["W{}".format(layer_num)] = (
-                np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
-            )
-            self.weights["b{}".format(layer_num)] = np.zeros((nodes, 1))
+            if i == 0:
+                self.weights["W1"] = (np.random.randn(layers[i], nx) *
+                                      np.sqrt(2 / nx))
+            else:
+                self.weights["W{}".format(i + 1)] = (
+                    np.random.randn(layers[i], layers[i - 1]) *
+                    np.sqrt(2 / layers[i - 1])
+                )
+
+            self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
