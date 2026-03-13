a0ad8e50245812ac8f18c87e12de982a84b71fd9
mammadow <mammadow.elnur@gmail.com>
2026-03-13 20:20:39 +0400

    sequential

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Builds a neural network with the Keras library"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Builds a neural network using Sequential"""
+    model = K.models.Sequential()
+
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(
+                units = layers[i],
+                activation = activations[i],
+                kernel_regularizer = K.regularizers.l2(lambtha),
+                input_shape = (nx,)
+            ))
+        else:
+            model.add(K.layers.Dense(
+                units = layers[i],
+                activation = activations[i],
+                kernel_regularizer = K.regularizers.l2(lambtha)
+            ))
+
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+
+    return model
