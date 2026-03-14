a5c48fade36050b32bbcaeda5cda25944d72dfa0
mammadow <mammadow.elnur@gmail.com>
2026-03-15 01:57:49 +0400

    train

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """Returns the History object generated after training the model"""
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
