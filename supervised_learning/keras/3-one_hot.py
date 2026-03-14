687a7bacf155ce0c8970c6772fd0e90366b1ec72
mammadow <mammadow.elnur@gmail.com>
2026-03-15 01:54:05 +0400

    one hot

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Converts a label vector into a one-hot matrix"""
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """Returns the one-hot matrix of labels"""
+    return K.utils.to_categorical(labels, num_classes=classes)
