84003053eee46cc88c141ad852ea5ad526d08cec
mammadow <mammadow.elnur@gmail.com>
2026-03-15 18:06:10 +0400

    save model

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Saves and loads a Keras model"""
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """Saves an entire model"""
+    network.save(filename)
+
+
+def load_model(filename):
+    """Loads an entire model"""
+    return K.models.load_model(filename)
