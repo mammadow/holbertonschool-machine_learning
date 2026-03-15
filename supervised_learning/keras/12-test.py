15d8928dfa6d80f86ea5969c4caebc8dfd3e1795
mammadow <mammadow.elnur@gmail.com>
2026-03-15 18:27:48 +0400

    test

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Tests a neural network"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Returns the loss and accuracy of the model"""
+    return network.evaluate(data, labels, verbose=verbose)
