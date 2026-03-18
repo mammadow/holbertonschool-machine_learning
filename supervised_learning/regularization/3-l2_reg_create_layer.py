d334f1605e56492c64ca3cddc6d41131bfa95549
mammadow <mammadow.elnur@gmail.com>
2026-03-18 17:41:03 +0400

    l2 reg create layer

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""creates a neural network layer with L2 regularization"""
+
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """creates a neural network layer with L2 regularization"""
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode="fan_avg"
+    )
+
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=tf.keras.regularizers.L2(lambtha)
+    )
+
+    return layer(prev)
