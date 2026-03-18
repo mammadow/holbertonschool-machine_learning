56ab805cecf7a2dbb421815e415fe9ceb185bec0
mammadow <mammadow.elnur@gmail.com>
2026-03-18 17:34:09 +0400

    fix

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""updates the weights and biases"""
+"""updates the weights and biases of a neural network using gradient descent
+with L2 regularization"""
 
 
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """updates the weights and biases of a neural network"""
+    """updates the weights and biases of a neural network using gradient
+    descent with L2 regularization"""
     m = Y.shape[1]
     dZ = cache["A" + str(L)] - Y
 
@@ -14,12 +16,13 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         A_prev = cache["A" + str(i - 1)]
         W = weights["W" + str(i)].copy()
 
-        dW = (np.matmul(dZ, A_prev.T) / m) + ((lambtha / m) * W)
+        dW = np.matmul(dZ, A_prev.T) / m + (lambtha / m) * W
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
         if i > 1:
-            dZ = np.matmul(W.T, dZ) *
-            (1 - np.power(cache["A" + str(i - 1)], 2))
+            dZ = np.matmul(W.T, dZ) * (
+                1 - np.power(cache["A" + str(i - 1)], 2)
+            )
 
-        weights["W" + str(i)] = weights["W" + str(i)] - alpha * dW
-        weights["b" + str(i)] = weights["b" + str(i)] - alpha * db
+        weights["W" + str(i)] -= alpha * dW
+        weights["b" + str(i)] -= alpha * db
