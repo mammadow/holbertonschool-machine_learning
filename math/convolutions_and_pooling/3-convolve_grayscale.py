fd99eb41c6990be7cac25160324de6b42c37d138
mammadow <mammadow.elnur@gmail.com>
2026-03-28 03:56:01 +0400

    convolve grayscale

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,8 +11,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = (((h - 1) * sh + kh - h) + 1) // 2
+        pw = (((w - 1) * sw + kw - w) + 1) // 2
     elif padding == 'valid':
         ph = 0
         pw = 0
