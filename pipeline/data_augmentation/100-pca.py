ae86520bb7a3aa2f0d872c3a77b5abbb127f9865
mammadow <mammadow.elnur@gmail.com>
2026-04-11 16:26:30 +0400

    pca

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -7,26 +7,26 @@ import tensorflow as tf
 def pca_color(image, alphas):
     """Perform PCA color augmentation"""
     image = tf.cast(image, tf.float32)
-    flat = tf.reshape(image, (-1, 3))
 
-    var = tf.math.reduce_variance(flat, axis=0)
-    scaling_factor = tf.sqrt(3.0 / tf.reduce_sum(var))
-    flat = flat * scaling_factor
+    flat = tf.reshape(image, (-1, 3))
 
     mean = tf.reduce_mean(flat, axis=0)
     centered = flat - mean
 
     n = tf.cast(tf.shape(centered)[0], tf.float32)
-    cov = tf.matmul(centered, centered, transpose_a=True) / (n - 1.0)
+    cov = tf.matmul(centered, centered, transpose_a=True) / (n - 1)
 
-    s, u, _ = tf.linalg.svd(cov)
+    eigvals, eigvecs = tf.linalg.eigh(cov)
 
+    eigvals = tf.reverse(eigvals, axis=[0])
+    eigvecs = tf.reverse(eigvecs, axis=[1])
     alphas = tf.cast(alphas, tf.float32)
-    delta = tf.linalg.matvec(u, alphas * s)
-    delta = tf.cast(delta * 255.0, tf.int32)
-    delta = tf.reshape(delta, (1, 1, 3))
+    delta = tf.matmul(eigvecs, tf.reshape(alphas * eigvals, (3, 1)))
+    delta = tf.reshape(delta, (1, 3))
 
-    out = tf.cast(image, tf.int32) + delta
+    flat = flat + delta
+    out = tf.reshape(flat, tf.shape(image))
+
     out = tf.clip_by_value(out, 0, 255)
 
     return tf.cast(out, tf.uint8)
