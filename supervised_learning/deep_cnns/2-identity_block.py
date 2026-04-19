75e41cbeb97d3c5241f8dd184f52bfcd117f2f7d
mammadow <mammadow.elnur@gmail.com>
2026-04-19 12:52:44 +0400

    add readme and identity block

---
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Identity block for ResNet"""
+from tensorflow import keras as K
+
+
+def identity_block(A_prev, filters):
+    """Builds an identity block as described in Resnet"""
+    F11, F3, F12 = filters
+    init = K.initializers.he_normal(seed=0)
+    X = K.layers.Conv2D(filters=F11,
+                        kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer=init)(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    X = K.layers.Conv2D(filters=F3,
+                        kernel_size=(3, 3),
+                        padding='same',
+                        kernel_initializer=init)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+
+    X = K.layers.Conv2D(filters=F12,
+                        kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer=init)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Add()([X, A_prev])
+    X = K.layers.Activation('relu')(X)
+
+    return X
