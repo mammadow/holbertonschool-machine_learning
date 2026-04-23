c8bf43f442a0cc4691a93befb98685686dd9e8af
mammadow <mammadow.elnur@gmail.com>
2026-04-23 19:51:42 +0400

    transfer

---
+++ supervised_learning/transfer_learning/0-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from tensorflow import keras as K
+transfer = __import__('0-transfer')
+
+preprocess_data = transfer.preprocess_data
+
+_, (X, Y) = K.datasets.cifar10.load_data()
+X_p, Y_p = preprocess_data(X, Y)
+model = K.models.load_model('cifar10.h5', safe_mode=False)
+model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
