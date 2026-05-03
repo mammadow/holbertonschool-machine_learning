yolo fix boxes calculation

supervised_learning/object_detection/1-yolo.py
@@ -1,68 +1,72 @@
 #!/usr/bin/env python3
-"""YOLO class"""
+"""YOLO object detection"""
 
 import tensorflow.keras as K
 import numpy as np
 
 
 class Yolo:
-    """YOLO v3 model"""
+    """YOLO v3 object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Initialize YOLO"""
         self.model = K.models.load_model(model_path)
-
-        with open(classes_path, 'r') as f:
+        with open(classes_path, "r") as f:
             self.class_names = [line.strip() for line in f]
-
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """Process outputs"""
+        """Process YOLO outputs"""
         boxes = []
         box_confidences = []
         box_class_probs = []
 
-        input_h = int(self.model.input.shape[1])
-        input_w = int(self.model.input.shape[2])
         image_h, image_w = image_size
 
         for i, output in enumerate(outputs):
             grid_h, grid_w, anchor_boxes, _ = output.shape
 
-            t_x = output[..., 0]
-            t_y = output[..., 1]
-            t_w = output[..., 2]
-            t_h = output[..., 3]
+            tx = output[..., 0]
+            ty = output[..., 1]
+            tw = output[..., 2]
+            th = output[..., 3]
 
-            c_x = np.arange(grid_w)
-            c_y = np.arange(grid_h)
-            c_x, c_y = np.meshgrid(c_x, c_y)
+            confidence = 1 / (1 + np.exp(-output[..., 4:5]))
+            class_probs = 1 / (1 + np.exp(-output[..., 5:]))
 
-            c_x = np.expand_dims(c_x, axis=-1)
-            c_y = np.expand_dims(c_y, axis=-1)
+            cx = np.arange(grid_w)
+            cy = np.arange(grid_h)
 
-            b_x = (1 / (1 + np.exp(-t_x)) + c_x) / grid_w
-            b_y = (1 / (1 + np.exp(-t_y)) + c_y) / grid_h
+            cx = cx.reshape(1, grid_w, 1)
+            cy = cy.reshape(grid_h, 1, 1)
 
-            anchors = self.anchors[i]
-            p_w = anchors[:, 0].reshape(1, 1, anchor_boxes)
-            p_h = anchors[:, 1].reshape(1, 1, anchor_boxes)
+            bx = (1 / (1 + np.exp(-tx)) + cx) / grid_w
+            by = (1 / (1 + np.exp(-ty)) + cy) / grid_h
 
-            b_w = (np.exp(t_w) * p_w) / input_w
-            b_h = (np.exp(t_h) * p_h) / input_h
+            anchor_w = self.anchors[i, :, 0].reshape(1, 1, anchor_boxes)
+            anchor_h = self.anchors[i, :, 1].reshape(1, 1, anchor_boxes)
+
+            input_w = self.model.input.shape[1]
+            input_h = self.model.input.shape[2]
+
+            bw = (np.exp(tw) * anchor_w) / input_w
+            bh = (np.exp(th) * anchor_h) / input_h
+
+            x1 = (bx - bw / 2) * image_w
+            y1 = (by - bh / 2) * image_h
+            x2 = (bx + bw / 2) * image_w
+            y2 = (by + bh / 2) * image_h
 
             box = np.zeros(output[..., :4].shape)
-
-            box[..., 0] = (b_x - b_w / 2) * image_w
-            box[..., 1] = (b_y - b_h / 2) * image_h
-            box[..., 2] = (b_x + b_w / 2) * image_w
-            box[..., 3] = (b_y + b_h / 2) * image_h
+            box[..., 0] = x1
+            box[..., 1] = y1
+            box[..., 2] = x2
+            box[..., 3] = y2
 
             boxes.append(box)
-            box_confidences.append(1 / (1 + np.exp(-output[..., 4:5])))
-            box_class_probs.append(1 / (1 + np.exp(-output[..., 5:])))
+            box_confidences.append(confidence)
+            box_class_probs.append(class_probs)
 
         return boxes, box_confidences, box_class_probs