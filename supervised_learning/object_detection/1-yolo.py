d24cf3390c4142876e29b8e347471d08372cb551
mammadow <mammadow.elnur@gmail.com>
2026-05-03 16:12:51 +0400

    yolo

---
+++ supervised_learning/object_detection/1-yolo.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""YOLO class"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """YOLO v3 model"""
+
+    def __init__(self, model_path, classes_path,
+                 class_t, nms_t, anchors):
+        """Initialize YOLO"""
+
+        self.model = K.models.load_model(model_path)
+
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, x):
+        """Sigmoid"""
+        return 1 / (1 + np.exp(-x))
+
+    def process_outputs(self, outputs, image_size):
+        """Process outputs"""
+
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+
+        input_h = self.model.input.shape[1]
+        input_w = self.model.input.shape[2]
+        img_h, img_w = image_size
+
+        for i, output in enumerate(outputs):
+            grid_h, grid_w, anchor_boxes, _ = output.shape
+
+            tx = output[..., 0]
+            ty = output[..., 1]
+            tw = output[..., 2]
+            th = output[..., 3]
+
+            box_conf = self.sigmoid(output[..., 4:5])
+            class_probs = self.sigmoid(output[..., 5:])
+
+            cx = np.arange(grid_w)
+            cy = np.arange(grid_h)
+            cx, cy = np.meshgrid(cx, cy)
+
+            cx = np.expand_dims(cx, axis=-1)
+            cy = np.expand_dims(cy, axis=-1)
+
+            bx = (self.sigmoid(tx) + cx) / grid_w
+            by = (self.sigmoid(ty) + cy) / grid_h
+
+            anchors = self.anchors[i]
+            pw = anchors[:, 0]
+            ph = anchors[:, 1]
+
+            pw = pw.reshape((1, 1, anchor_boxes))
+            ph = ph.reshape((1, 1, anchor_boxes))
+
+            bw = (np.exp(tw) * pw) / input_w
+            bh = (np.exp(th) * ph) / input_h
+
+            x1 = (bx - bw / 2) * img_w
+            y1 = (by - bh / 2) * img_h
+            x2 = (bx + bw / 2) * img_w
+            y2 = (by + bh / 2) * img_h
+
+            box = np.stack([x1, y1, x2, y2], axis=-1)
+
+            boxes.append(box)
+            box_confidences.append(box_conf)
+            box_class_probs.append(class_probs)
+
+        return boxes, box_confidences, box_class_probs
