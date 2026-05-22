e30363392b821976d06929a970ea053b6e363a93
mammadow <mammadow.elnur@gmail.com>
2026-05-22 19:10:57 +0400

    pca fix

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,5 +8,11 @@ def pca(X, var=0.95):
     ev = S ** 2
     vr = ev / np.sum(ev)
     cvr = np.cumsum(vr)
-    nd = np.argmax(cvr >= var) + 1
+
+    nd = 0
+    for v in cvr:
+        nd += 1
+        if v >= var:
+            break
+
     return Vt.T[:, :nd]
