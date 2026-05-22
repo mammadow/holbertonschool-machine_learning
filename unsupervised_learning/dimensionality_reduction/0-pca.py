09272d7624f5c38bdda30a74628cd6e566929dc2
mammadow <mammadow.elnur@gmail.com>
2026-05-22 19:12:24 +0400

    pca fix

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -3,16 +3,10 @@ import numpy as np
 
 
 def pca(X, var=0.95):
-    """Performs PCA and returns projection matrix."""
+    """Performs PCA on a dataset."""
     _, S, Vt = np.linalg.svd(X, full_matrices=False)
     ev = S ** 2
     vr = ev / np.sum(ev)
     cvr = np.cumsum(vr)
-
-    nd = 0
-    for v in cvr:
-        nd += 1
-        if v >= var:
-            break
-
+    nd = np.argmax(cvr >= var) + 1
     return Vt.T[:, :nd]
