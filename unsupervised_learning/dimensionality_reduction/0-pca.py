a9ed8417ddd2909a1e76c1f82478b853c9b9f509
mammadow <mammadow.elnur@gmail.com>
2026-05-22 18:58:31 +0400

    pca

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Performs PCA and returns projection matrix."""
+    _, S, Vt = np.linalg.svd(X, full_matrices=False)
+    ev = S ** 2
+    vr = ev / np.sum(ev)
+    cvr = np.cumsum(vr)
+    nd = np.argmax(cvr >= var) + 1
+    return Vt.T[:, :nd]
