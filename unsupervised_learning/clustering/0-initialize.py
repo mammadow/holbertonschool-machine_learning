44468b613e00cd52b4597cb62f0568a761877934
mammadow <mammadow.elnur@gmail.com>
2026-05-23 21:36:14 +0400

    fix

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -6,7 +6,11 @@ import numpy as np
 
 def initialize(X, k):
     """Initializes cluster centroids for K-means"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2 or k <= 0:
+    if not isinstance(X, np.ndarray):
+        return None
+    if X.ndim != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
         return None
 
     try:
