7d4116b33a65feeb40ec86ce074bf4747b88d32f
mammadow <mammadow.elnur@gmail.com>
2026-05-24 23:40:30 +0400

    kmeans sklearn

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """Performs K-means using sklearn"""
-import numpy as np
 from sklearn.cluster import KMeans
 
 
 def kmeans(X, k):
     """Applies KMeans clustering from sklearn"""
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None, None
     if type(k) is not int or k <= 0:
         return None, None
 
