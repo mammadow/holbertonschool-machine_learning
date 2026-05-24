8b9a41ccd637313d050d50892437f247c327e253
mammadow <mammadow.elnur@gmail.com>
2026-05-24 23:43:11 +0400

    gmm

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Fits a Gaussian Mixture Model using sklearn"""
-import numpy as np
 from sklearn.mixture import GaussianMixture
 
 
 def gmm(X, k):
     """Fits a GMM and returns parameters and predictions"""
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+    if X is None or len(X.shape) != 2:
         return None, None, None, None, None
     if type(k) is not int or k <= 0:
         return None, None, None, None, None
