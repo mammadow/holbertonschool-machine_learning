227778762acfbba3bbd1eac1033fb260a388cd59
mammadow <mammadow.elnur@gmail.com>
2026-05-25 01:02:34 +0400

    format

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Performs the expectation maximization for a GMM"""
-
+"""Performs the expectation maximization for a GMM."""
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -8,7 +7,7 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """Performs the expectation maximization for a GMM"""
+    """Performs the expectation maximization for a GMM."""
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None, None, None
     if type(k) is not int or k <= 0:
@@ -19,26 +18,21 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None, None
     if type(verbose) is not bool:
         return None, None, None, None, None
-
     i = 0
     prev_ll = 0
-
     pi, m, S = initialize(X, k)
     g, ll = expectation(X, pi, m, S)
-
     while i < iterations:
         if np.abs(prev_ll - ll) <= tol:
             break
         prev_ll = ll
-
         if verbose and (i % 10 == 0):
-            print("Log Likelihood after {} iterations: {:.5f}".format(i, ll))
-
+            print("Log Likelihood after {} iterations: {}".format(
+                i, round(ll, 5)))
         pi, m, S = maximization(X, g)
         g, ll = expectation(X, pi, m, S)
         i += 1
-
     if verbose:
-        print("Log Likelihood after {} iterations: {:.5f}".format(i, ll))
-
+        print("Log Likelihood after {} iterations: {}".format(
+            i, round(ll, 5)))
     return pi, m, S, g, ll
