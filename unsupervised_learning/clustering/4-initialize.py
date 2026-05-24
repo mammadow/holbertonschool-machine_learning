20b651f346ccb7ca9e6c3eed5be2f7bd5cc7b2ee
mammadow <mammadow.elnur@gmail.com>
2026-05-24 18:52:08 +0400

    init gmm

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Initializes GMM parameters"""
+
+import numpy as np
+
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """Initializes variables for a Gaussian Mixture Model"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None
+
+    try:
+        n, d = X.shape
+
+        pi = np.full(k, 1 / k)
+
+        _, m = kmeans(X, k)
+
+        S = np.zeros((k, d, d))
+        S[:, :, :] = np.eye(d)
+
+        return pi, m, S
+
+    except Exception:
+        return None, None, None
