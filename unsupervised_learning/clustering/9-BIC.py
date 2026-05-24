2704aa93650403691456dfb142531ef2e3734d5e
mammadow <mammadow.elnur@gmail.com>
2026-05-25 00:49:09 +0400

    bic

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-"""Bayesian Information Criterion for Gaussian Mixture Models"""
+"""
+Finds the best number of clusters for a GMM using BIC.
+"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """Find the best number of clusters for a GMM using BIC"""
+    """
+    Computes the best k using Bayesian Information Criterion.
+    """
 
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0 or X.shape[0] <= kmin:
-        return None, None, None, None
-
     if kmax is None:
         kmax = X.shape[0]
-
-    if not isinstance(kmax, int) or kmax <= 0 or X.shape[0] <= kmax:
+    if not isinstance(kmin, int) or kmin <= 0 or kmin >= X.shape[0]:
+        return None, None, None, None
+    if not isinstance(kmax, int) or kmax <= 0 or kmax > X.shape[0]:
+        return None, None, None, None
+    if kmin > kmax:
         return None, None, None, None
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None
@@ -26,32 +30,35 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
 
     n, d = X.shape
 
-    all_pis = []
-    all_ms = []
-    all_Ss = []
-    all_lkhds = []
-    all_bs = []
+    pis = []
+    means = []
+    covs = []
+    logs = []
+    bics = []
 
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, lkhd = expectation_maximization(
+        pi, m, S, g, log_l = expectation_maximization(
             X, k, iterations, tol, verbose
         )
 
-        if pi is None or m is None or S is None:
+        if pi is None:
             return None, None, None, None
 
-        all_pis.append(pi)
-        all_ms.append(m)
-        all_Ss.append(S)
-        all_lkhds.append(lkhd)
+        pis.append(pi)
+        means.append(m)
+        covs.append(S)
+        logs.append(log_l)
 
         p = (k * d * (d + 1) / 2) + (d * k) + (k - 1)
-        all_bs.append(p * np.log(n) - 2 * lkhd)
+        bic = p * np.log(n) - 2 * log_l
+        bics.append(bic)
 
-    all_lkhds = np.array(all_lkhds)
-    all_bs = np.array(all_bs)
+    logs = np.array(logs)
+    bics = np.array(bics)
 
-    best_k = np.argmin(all_bs)
-    best_result = (all_pis[best_k], all_ms[best_k], all_Ss[best_k])
+    best_idx = np.argmin(bics)
 
-    return best_k + kmin, best_result, all_lkhds, all_bs
+    best_k = kmin + best_idx
+    best_result = (pis[best_idx], means[best_idx], covs[best_idx])
+
+    return best_k, best_result, logs, bics
