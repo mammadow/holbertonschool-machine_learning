57a8b2dbcf75196cf246a9cccc0d16fcc4fb5aab
mammadow <mammadow.elnur@gmail.com>
2026-05-24 23:50:47 +0400

    bic

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""Finds best number of clusters for a GMM using BIC"""
+"""Computes BIC for GMM model selection"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """Performs model selection using Bayesian Information Criterion"""
+    """Selects best K using Bayesian Information Criterion"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None, None
 
     n, d = X.shape
+
     if kmax is None:
         kmax = n
 
@@ -21,28 +22,36 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     b = np.zeros(len(ks))
 
     best_k = None
-    best_l = None
+    best_bic = np.inf
     best_result = None
 
     for i, k in enumerate(ks):
-        pi, m, S, g, log_l = expectation_maximization(
-            X, k, iterations=iterations, tol=tol, verbose=verbose
-        )
+        try:
+            pi, m, S, g, log_l = expectation_maximization(
+                X, k, iterations=iterations, tol=tol, verbose=verbose
+            )
+        except Exception:
+            continue
+
+        if log_l is None:
+            continue
 
         l[i] = log_l
 
-        p = (k - 1) + (k * d) + (k * d * (d + 1)) // 2
-        b[i] = p * np.log(n) - 2 * log_l
+        p = k - 1 + k * d + (k * d * (d + 1)) // 2
+        bic = p * np.log(n) - 2 * log_l
+        b[i] = bic
 
-        if best_l is None or b[i] < b[ks == best_k][0] if best_k is not None else True:
+        if bic < best_bic:
+            best_bic = bic
             best_k = k
-            best_l = log_l
             best_result = (pi, m, S)
 
-    best_idx = np.argmin(b)
-    best_k = ks[best_idx]
-    best_result = expectation_maximization(
+    if best_k is None:
+        return None, None, None, None
+
+    pi, m, S, g, log_l = expectation_maximization(
         X, best_k, iterations=iterations, tol=tol, verbose=False
-    )[:3]
+    )
 
     return best_k, best_result, l, b
