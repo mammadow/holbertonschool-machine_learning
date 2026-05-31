f7eb5d9fca63b46971092ee849e92aed0bfe2b49
mammadow <mammadow.elnur@gmail.com>
2026-05-31 23:45:39 +0400

    bayes acquisition

---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Bayesian Optimization - Acquisition function
+"""
+
+import numpy as np
+from scipy.stats import norm
+
+
+class BayesianOptimization:
+    """
+    Bayesian Optimization using Gaussian Process
+    """
+
+    def __init__(
+        self,
+        f,
+        X_init,
+        Y_init,
+        bounds,
+        ac_samples,
+        l=1,
+        sigma_f=1,
+        xsi=0.01,
+        minimize=True
+    ):
+        """
+        Constructor
+        """
+        self.f = f
+
+        GP = __import__('2-gp').GaussianProcess
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+
+        self.X_s = np.linspace(
+            bounds[0],
+            bounds[1],
+            ac_samples
+        ).reshape(-1, 1)
+
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """
+        Calculates Expected Improvement and best next sample
+        """
+
+        mu, var = self.gp.predict(self.X_s)
+        sigma = np.sqrt(var)
+
+        if self.minimize:
+            best = np.min(self.gp.Y)
+            imp = best - mu - self.xsi
+        else:
+            best = np.max(self.gp.Y)
+            imp = mu - best - self.xsi
+
+        Z = np.zeros_like(mu)
+
+        mask = sigma > 0
+        Z[mask] = imp[mask] / sigma[mask]
+
+        EI = np.zeros_like(mu)
+
+        EI[mask] = (
+            imp[mask] * norm.cdf(Z[mask]) +
+            sigma[mask] * norm.pdf(Z[mask])
+        )
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, EI.reshape(-1)
