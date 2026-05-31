b8a3af112b513e3f60abf3914b22d73d5e1980ba
mammadow <mammadow.elnur@gmail.com>
2026-05-31 23:29:30 +0400

    gp

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Gaussian Process module"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix
+        """
+        sqdist = (X1 - X2.T) ** 2
+
+        return (
+            self.sigma_f ** 2
+            * np.exp(-sqdist / (2 * self.l ** 2))
+        )
