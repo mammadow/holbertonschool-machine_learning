5c3406468f66a8b754a7adee5a2690bda8ecde85
mammadow <mammadow.elnur@gmail.com>
2026-05-31 23:36:49 +0400

    bayes optimization

---
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Bayesian Optimization initialization
+"""
+
+import numpy as np
+
+
+class BayesianOptimization:
+    """
+    Bayesian Optimization for 1D noiseless Gaussian Process
+    """
+
+    def __init__(
+        self,
+        f,
+        X_init,
+        Y_init,
+        bounds,
+        ac_samples,
+        l=1,
+        sigma_f=1,
+        xsi=0.01,
+        minimize=True
+    ):
+        """
+        Constructor
+        """
+
+        self.f = f
+
+        GP = __import__('2-gp').GaussianProcess
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+
+        self.X_s = np.linspace(
+            bounds[0],
+            bounds[1],
+            ac_samples
+        ).reshape(-1, 1)
+
+        self.xsi = xsi
+        self.minimize = minimize
