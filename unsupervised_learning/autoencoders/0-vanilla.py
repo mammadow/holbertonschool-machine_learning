48010774e59d04333cc901b6994729c203b1cfbd
mammadow <mammadow.elnur@gmail.com>
2026-06-07 00:51:47 +0400

    vanilla autoencoder

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Vanilla autoencoder implementation."""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates a vanilla autoencoder model."""
+
+    input_layer = keras.Input(shape=(input_dims,))
+
+    x = input_layer
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation='relu')(x)
+
+    latent = keras.layers.Dense(latent_dims, activation='relu')(x)
+
+    encoder = keras.Model(inputs=input_layer, outputs=latent)
+
+    latent_input = keras.Input(shape=(latent_dims,))
+    x = latent_input
+
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation='relu')(x)
+
+    output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+
+    decoder = keras.Model(inputs=latent_input, outputs=output)
+
+    reconstructed = decoder(encoder(input_layer))
+    auto = keras.Model(inputs=input_layer, outputs=reconstructed)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
