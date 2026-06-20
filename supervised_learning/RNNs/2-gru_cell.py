da122a43eac2ae73a341f6160cd66de486330777
mammadow <mammadow.elnur@gmail.com>
2026-06-20 20:48:43 +0400

    gru cell

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""GRU Cell implementation."""
+
 import numpy as np
 
 
@@ -18,7 +20,7 @@ class GRUCell:
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """Performs forward propagation for one time step."""
+        """Perform forward propagation for one time step."""
         concat = np.concatenate((h_prev, x_t), axis=1)
 
         z = self._sigmoid(np.matmul(concat, self.Wz) + self.bz)
