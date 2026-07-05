5001f9c3b1f84eb67515232b41f4c6f068447d50
mammadow <mammadow.elnur@gmail.com>
2026-07-05 20:03:37 +0400

    fasttext

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""FastText model."""
+
+import gensim
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5,
+                   negative=5, window=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
+    """Create and train a FastText model."""
+    model = gensim.models.fasttext.FastText(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=int(not cbow),
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
+
+    model.train(
+        corpus_iterable=sentences,
+        total_examples=len(sentences),
+        epochs=epochs
+    )
+
+    return model
