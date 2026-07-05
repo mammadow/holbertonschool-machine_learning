a0775164a006c976a39a982113c584ebce5aeecb
mammadow <mammadow.elnur@gmail.com>
2026-07-05 18:40:55 +0400

    word2vec

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Module that builds and trains a gensim word2vec model"""
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                     negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Creates, builds, and trains a gensim word2vec model"""
     sg = 0 if cbow else 1
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
