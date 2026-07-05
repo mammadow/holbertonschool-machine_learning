bb66356299b0b758c16d1aa108ab69723c4eaff6
mammadow <mammadow.elnur@gmail.com>
2026-07-05 18:33:36 +0400

    BoW

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python3
-"""Bag of Words embedding module."""
+"""Bag of Words embedding."""
 
 import re
 import numpy as np
 
 
 def bag_of_words(sentences, vocab=None):
-    """Creates a bag of words embedding matrix."""
-
+    """Create a bag of words embedding matrix."""
     processed = []
 
     for sentence in sentences:
         sentence = sentence.lower()
-        # Remove possessive 's
         sentence = re.sub(r"'s\b", "", sentence)
-        # Remove remaining punctuation
         sentence = re.sub(r"[^a-z0-9\s]", "", sentence)
         processed.append(sentence.split())
 
     if vocab is None:
-        features = sorted(set(
+        features = sorted({
             word
             for sentence in processed
             for word in sentence
-        ))
+        })
     else:
-        features = sorted(vocab)
+        features = list(vocab)
 
     word_to_index = {
         word: i
         for i, word in enumerate(features)
     }
 
-    embeddings = np.zeros(
-        (len(sentences), len(features)),
-        dtype=int
-    )
+    embeddings = np.zeros((len(sentences), len(features)), dtype=int)
 
     for i, sentence in enumerate(processed):
         for word in sentence:
