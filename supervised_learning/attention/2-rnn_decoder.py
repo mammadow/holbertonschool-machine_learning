2ca366ba3ffd5b48e90bdf2eb02161ccc9bab7e0
mammadow <mammadow.elnur@gmail.com>
2026-07-15 15:24:44 +0400

    rnn decoder

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -13,15 +13,10 @@ class RNNDecoder(tf.keras.layers.Layer):
         """Initialize the decoder."""
         super().__init__()
 
-        self.attention = SelfAttention(units)
-
-        self.embedding = tf.keras.layers.Embedding(
-            input_dim=vocab,
-            output_dim=embedding
-        )
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
 
         self.gru = tf.keras.layers.GRU(
-            units=units,
+            units,
             return_sequences=True,
             return_state=True,
             recurrent_initializer="glorot_uniform"
@@ -29,13 +24,15 @@ class RNNDecoder(tf.keras.layers.Layer):
 
         self.F = tf.keras.layers.Dense(vocab)
 
+        self.attention = SelfAttention(units)
+
     def call(self, x, s_prev, hidden_states):
         """Perform one decoding step."""
         context, _ = self.attention(s_prev, hidden_states)
 
         x = self.embedding(x)
 
-        context = tf.expand_dims(context, axis=1)
+        context = tf.expand_dims(context, 1)
 
         x = tf.concat([context, x], axis=-1)
 
