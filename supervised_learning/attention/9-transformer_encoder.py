62a75ee5489d4058edf3dec252067e1eca02b81a
mammadow <mammadow.elnur@gmail.com>
2026-07-15 15:09:41 +0400

    transformer encoder

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -2,8 +2,8 @@
 """Transformer Encoder"""
 
 import tensorflow as tf
-positional_encoding = __import__("4-positional_encoding").positional_encoding
-EncoderBlock = __import__("7-transformer_encoder_block").EncoderBlock
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
