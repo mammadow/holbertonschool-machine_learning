9e74a6263e5229c6796113f0841a0a335be10689
mammadow <mammadow.elnur@gmail.com>
2026-07-15 15:03:35 +0400

    transformer encoder block

---
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Transformer Encoder Block"""
+
+import tensorflow as tf
+
+MultiHeadAttention = __import__(
+    '6-multihead_attention'
+).MultiHeadAttention
+
+
+class EncoderBlock(tf.keras.layers.Layer):
+    """Creates an encoder block for a transformer."""
+
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """Initialize the encoder block."""
+        super().__init__()
+
+        self.mha = MultiHeadAttention(dm, h)
+
+        self.dense_hidden = tf.keras.layers.Dense(
+            hidden,
+            activation='relu'
+        )
+        self.dense_output = tf.keras.layers.Dense(dm)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm2 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask=None):
+        """Forward pass."""
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(
+            attn_output,
+            training=training
+        )
+        out1 = self.layernorm1(x + attn_output)
+
+        ffn_output = self.dense_hidden(out1)
+        ffn_output = self.dense_output(ffn_output)
+        ffn_output = self.dropout2(
+            ffn_output,
+            training=training
+        )
+
+        return self.layernorm2(out1 + ffn_output)
