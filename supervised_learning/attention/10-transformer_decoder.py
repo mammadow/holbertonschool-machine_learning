06e75ae4426b7e7c36ec761760b388f5ff836585
mammadow <mammadow.elnur@gmail.com>
2026-07-15 15:08:44 +0400

    transformer decoder

---
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Transformer Decoder"""
+
+import tensorflow as tf
+from tensorflow import keras
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
+
+
+class Decoder(keras.layers.Layer):
+    """Transformer decoder"""
+
+    def __init__(self, N, dm, h, hidden, target_vocab,
+                 max_seq_len, drop_rate=0.1):
+        """Initialize the decoder"""
+        super().__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = keras.layers.Embedding(target_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [
+            DecoderBlock(dm, h, hidden, drop_rate)
+            for _ in range(N)
+        ]
+        self.dropout = keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training,
+             look_ahead_mask, padding_mask):
+        """Forward pass"""
+        seq_len = tf.shape(x)[1]
+
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:seq_len]
+
+        x = self.dropout(x, training=training)
+
+        for block in self.blocks:
+            x = block(
+                x,
+                encoder_output,
+                training,
+                look_ahead_mask,
+                padding_mask
+            )
+
+        return x
