d7afa2097cba74a9d3e08fea7b1334ca7281b31d
mammadow <mammadow.elnur@gmail.com>
2026-07-19 23:32:50 +0400

    dataset encode tokens

---
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""Dataset class."""
+
+from setup import load_pt2en
+import transformers
+
+
+class Dataset:
+    """Dataset."""
+
+    def __init__(self):
+        """Initialize dataset."""
+        self.data_train = load_pt2en("train")
+        self.data_valid = load_pt2en("validation")
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
+
+    def tokenize_dataset(self, data):
+        """Train tokenizers."""
+
+        def pt_iterator():
+            """PT iterator."""
+            for pt, _ in data:
+                yield pt.numpy().decode("utf-8")
+
+        def en_iterator():
+            """EN iterator."""
+            for _, en in data:
+                yield en.numpy().decode("utf-8")
+
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased"
+        )
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            "bert-base-uncased"
+        )
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            pt_iterator(),
+            vocab_size=2 ** 13
+        )
+
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            en_iterator(),
+            vocab_size=2 ** 13
+        )
+
+        return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """Encode sentences."""
+        pt_tokens = [self.tokenizer_pt.vocab_size]
+        pt_tokens += self.tokenizer_pt.encode(
+            pt.numpy().decode("utf-8"),
+            add_special_tokens=False
+        )
+        pt_tokens.append(self.tokenizer_pt.vocab_size + 1)
+
+        en_tokens = [self.tokenizer_en.vocab_size]
+        en_tokens += self.tokenizer_en.encode(
+            en.numpy().decode("utf-8"),
+            add_special_tokens=False
+        )
+        en_tokens.append(self.tokenizer_en.vocab_size + 1)
+
+        return pt_tokens, en_tokens
